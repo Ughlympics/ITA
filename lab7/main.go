8d504cf90f9cfb089015db122aa032c824b78fa5
Ughlympics <kipgora@gmail.com>
2025-05-07 15:27:40 +0300

    Problem 2

--- lab7/main.go
+++ lab7/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"lab7/problem"
 	"os"
@@ -9,34 +8,56 @@ import (
 )
 
 func main() {
-	file, err := os.Open("input.dat")
+	// file, err := os.Open("input.dat")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer file.Close()
+
+	// scanner := bufio.NewScanner(file)
+	// scanner.Scan()
+	// var n int
+	// fmt.Sscanf(scanner.Text(), "%d", &n)
+
+	// var patterns []string
+	// for scanner.Scan() {
+	// 	line := strings.TrimSpace(scanner.Text())
+	// 	if len(line) > 0 {
+	// 		patterns = append(patterns, line)
+	// 	}
+	// }
+
+	// output := problem.BuildPrefixTree(patterns)
+
+	// outFile, err := os.Create("output.dat")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer outFile.Close()
+
+	// for _, line := range output {
+	// 	outFile.WriteString(line + "\n")
+	// }
+
+	data, err := os.ReadFile("input2.dat")
 	if err != nil {
 		panic(err)
 	}
-	defer file.Close()
+	lines := strings.Split(strings.TrimSpace(string(data)), "\n")
 
-	scanner := bufio.NewScanner(file)
-	scanner.Scan()
-	var n int
-	fmt.Sscanf(scanner.Text(), "%d", &n)
+	text := lines[0]
+	patternCount := problem.ParseInt(lines[1])
+	patterns := lines[2 : 2+patternCount]
 
-	var patterns []string
-	for scanner.Scan() {
-		line := strings.TrimSpace(scanner.Text())
-		if len(line) > 0 {
-			patterns = append(patterns, line)
-		}
+	indices := problem.MatchPatternsInText(text, patterns)
+
+	var output []string
+	for _, idx := range indices {
+		output = append(output, fmt.Sprintf("%d", idx))
 	}
 
-	output := problem.BuildPrefixTree(patterns)
-
-	outFile, err := os.Create("output.dat")
+	err = os.WriteFile("output2.dat", []byte(strings.Join(output, " ")), 0644)
 	if err != nil {
 		panic(err)
-	}
-	defer outFile.Close()
-
-	for _, line := range output {
-		outFile.WriteString(line + "\n")
 	}
 }
--- lab7/problem/second.go
+++ lab7/problem/second.go
@@ -1,1 +1,53 @@
 package problem
+
+type TrieNode struct {
+	children    map[byte]*TrieNode
+	isEndOfWord bool
+}
+
+func NewTrieNode() *TrieNode {
+	return &TrieNode{children: make(map[byte]*TrieNode)}
+}
+
+func BuildPrefixTre(patterns []string) *TrieNode {
+	root := NewTrieNode()
+	for _, pattern := range patterns {
+		current := root
+		for i := 0; i < len(pattern); i++ {
+			char := pattern[i]
+			if current.children[char] == nil {
+				current.children[char] = NewTrieNode()
+			}
+			current = current.children[char]
+		}
+		current.isEndOfWord = true
+	}
+	return root
+}
+
+func MatchPatternsInText(text string, patterns []string) []int {
+	root := BuildPrefixTre(patterns)
+	var matchIndices []int
+
+	for i := 0; i < len(text); i++ {
+		current := root
+		position := i
+		for position < len(text) && current.children[text[position]] != nil {
+			current = current.children[text[position]]
+			if current.isEndOfWord {
+				matchIndices = append(matchIndices, i)
+				break
+			}
+			position++
+		}
+	}
+	return matchIndices
+}
+
+func ParseInt(s string) int {
+	result := 0
+	for _, ch := range s {
+		result = result*10 + int(ch-'0')
+	}
+	return result
+}
